599493c46ccb6cd6d3e9c934c156f340c2e36088
Lucas Costa <lucasfrct@gmail.com>
2024-04-06 17:20:40 -0300

    estruturando folders

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,33 @@
 # ## python para desenvolvimento
 
-# ! ## STAGE 1 - Maquina para armazenar as dependencias
-FROM python:3 AS environment-python-dependences
+# ! ## STAGE 1 - Maquina para armazenar as ferramentas
+FROM python:3 AS environment-python-tools
 
 WORKDIR /app
 
 # ## install tools
 RUN pip install chromadb
+RUN pip install openai
+RUN pip install numpy
+RUN pip install fastapi
+RUN pip install PyPDF2
+RUN pip install ollama
+RUN pip install unicorn
+RUN pip install pyvis
+RUN pip install scikit-learn
+
+# ! ## STAGE 1 - Maquina para armazenar as dependencias
+FROM environment-python-tools AS environment-python-dependences
+
+WORKDIR /app
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
-# ! ## STAGE 2 - Maquina para excutar o ambiente de desenvolvimento
+# ! ## STAGE 3 - Maquina para excutar o ambiente de desenvolvimento
 FROM environment-python-dependences AS environment-python-dev
+
+WORKDIR /app
 
 COPY . .
 
--- main.py
+++ main.py
@@ -3,24 +3,37 @@ import sys
 import time
 import threading
 
-from model import Model as Embedding
-from agent import Agent
- 
-def spinner():
-    spinner = ['|', '/', '-', '\\']
-    while True:
-        for symbol in spinner:
-            sys.stdout.write('\r' + symbol)
-            sys.stdout.flush()
-            time.sleep(0.1)
+from src import process
+from src.database import chromadb
+from src.display import chart
+from src.entities.agent import Agent
 
 
+sys.dont_write_bytecode = True
 def run():
+
+    # PATH_DOCS = "./docs"
     
+    # print("\n", "########## Iniciando registro de documentos ##########", "\n")
+    # documents = process.get_documents(PATH_DOCS)
+    # documents_size = len(documents)
+    # for i, document in enumerate(documents): 
+    #     print(f"    {i+1}/{documents_size}: {document.info}")
+
+    # print("\n", "########## Iniciando embedings de documentos ##########", "\n")
+    # models = process.embed_documents(documents)
+    # for i, model in enumerate(models): 
+    #     # chromadb.save_model(model)
+    #     chart(f"{model.doc.name}", model)
+    #     print(f"    {i+1}: {model.doc.info}")
+
+
+    ####################################################################
     agent = Agent()
     
     for line in sys.stdin:
         agent.question(line)
+
 
 if __name__ == "__main__":
     run()
