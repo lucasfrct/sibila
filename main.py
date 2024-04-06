c834474aaf87c20b1d15fb84bbe57883ba20e7c7
Lucas Costa <lucasfrct@gmail.com>
2024-04-06 17:38:20 -0300

    preparando para teste de start

--- main.py
+++ main.py
@@ -9,23 +9,22 @@ sys.dont_write_bytecode = True
 
 def run():
 
-    # PATH_DOCS = "./docs"
+    PATH_DOCS = "./docs"
     
-    # print("\n", "########## Iniciando registro de documentos ##########", "\n")
-    # documents = process.get_documents(PATH_DOCS)
-    # documents_size = len(documents)
-    # for i, document in enumerate(documents): 
-    #     print(f"    {i+1}/{documents_size}: {document.info}")
+    print("\n", "########## Iniciando registro de documentos ##########", "\n")
+    documents = process.get_documents(PATH_DOCS)
+    documents_size = len(documents)
+    for i, document in enumerate(documents): 
+        print(f"    {i+1}/{documents_size}: {document.info}")
 
-    # print("\n", "########## Iniciando embedings de documentos ##########", "\n")
-    # models = process.embed_documents(documents)
-    # for i, model in enumerate(models): 
-    #     # chromadb.save_model(model)
-    #     chart(f"{model.doc.name}", model)
-    #     print(f"    {i+1}: {model.doc.info}")
+    print("\n", "########## Iniciando embedings de documentos ##########", "\n")
+    models = process.embed_documents(documents)
+    for i, model in enumerate(models): 
+        # chromadb.save_model(model)
+        chart(f"{model.doc.name}", model)
+        print(f"    {i+1}: {model.doc.info}")
 
-
-    ####################################################################
+    print()
     agent = Agent()
     
     for line in sys.stdin:
